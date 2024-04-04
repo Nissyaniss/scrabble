62ef4e19d4d21f38363bbf017738a5d5f3297c01
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-04-04 10:31:50 +0200

    add ScrabbleApplicationConsole

---
+++ src/main/java/fr/unilim/scrabble/application/ScrabbleApplicationConsole.java
@@ -0,0 +1,9 @@
+package fr.unilim.scrabble.application;
+
+public class ScrabbleApplicationConsole {
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------");
+        System.out.println("-- Bienvenue dans notre magnifique jeu de scrabble ! --");
+        System.out.println("-------------------------------------------------------");
+    }
+}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module fr.unilim.scrabble {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens fr.unilim.scrabble to javafx.fxml;
-    exports fr.unilim.scrabble;
-}
