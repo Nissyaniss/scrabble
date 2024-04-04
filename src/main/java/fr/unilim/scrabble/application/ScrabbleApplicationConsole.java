35b7f83dcd0d1b40bcde36eda5c7820edb5cf75c
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-04-04 10:41:44 +0200

    add credit to myself

--- src/main/java/fr/unilim/scrabble/application/ScrabbleApplicationConsole.java
+++ src/main/java/fr/unilim/scrabble/application/ScrabbleApplicationConsole.java
@@ -4,6 +4,7 @@ public class ScrabbleApplicationConsole {
     public static void main(String[] args) {
         System.out.println("-------------------------------------------------------");
         System.out.println("-- Bienvenue dans notre magnifique jeu de scrabble ! --");
+        System.out.println("-- développé par Rayzeq                              --");
         System.out.println("-------------------------------------------------------");
     }
 }
