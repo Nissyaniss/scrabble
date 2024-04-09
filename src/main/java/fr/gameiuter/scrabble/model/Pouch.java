5f2b1ad040fc0711144fa3e9d6a8c9da77459471
Yaniss Lasbordes <yaniss.lasbordes@gmail.com>
2024-04-09 16:48:37 +0200

    Added Pouch implementation

--- src/main/java/fr/gameiuter/scrabble/model/Pouch.java
+++ src/main/java/fr/gameiuter/scrabble/model/Pouch.java
@@ -1,16 +1,27 @@
 package fr.gameiuter.scrabble.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Pouch {
     private List<Tile> pool;
+    private Random random = new Random();
+
+    public Pouch(){
+        this.pool = new ArrayList<Tile>();
+    }
 
     public Tile draw() {
-        // TODO
-        return null;
+        int size = pool.size();
+
+        if (size == 0)
+            return null;
+
+        return pool.get(this.random.nextInt(size));
     }
 
     public void putBack(Tile tile) {
-        // TODO
+        pool.add(tile);
     }
 }
