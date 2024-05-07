8966e41547efce5fe9cbe6299c582975030b4f79
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-05-07 17:24:12 +0200

    removed MarkSheet as it won't be used

--- src/main/java/fr/gameiuter/scrabble/model/Player.java
+++ src/main/java/fr/gameiuter/scrabble/model/Player.java
@@ -3,11 +3,9 @@ package fr.gameiuter.scrabble.model;
 public class Player {
     private final String name;
     private final Rack rack;
-    private final MarkSheet markSheet;
 
     public Player(String name, Pouch pouch) {
         this.name = name;
         this.rack = new Rack(pouch);
-        this.markSheet = new MarkSheet();
     }
 }
