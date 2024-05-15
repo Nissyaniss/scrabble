bec3ef6b62b603c9c14606f9015dea9b810ea0d7
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-05-15 15:11:51 +0200

    add all squares in the board

--- src/main/java/fr/gameiuter/scrabble/model/Board.java
+++ src/main/java/fr/gameiuter/scrabble/model/Board.java
@@ -5,7 +5,7 @@ public class Board {
     public static final Integer MIDDLE = 7;
 
     private final Square[][] squares;
-    private Tile[][] placedTiles;
+    private final Tile[][] placedTiles;
 
     public Board() {
         this.squares = new Square[SIZE][SIZE];
@@ -16,6 +16,30 @@ public class Board {
             }
         }
         this.squares[MIDDLE][MIDDLE] = Square.START;
+        this.setSquareSymetrical(0, 0, Square.TRIPLE_WORD);
+        this.setSquareSymetrical(MIDDLE, 0, Square.TRIPLE_WORD);
+        this.setSquareSymetrical(3, 0, Square.DOUBLE_LETTER);
+        this.setSquareSymetrical(1, 1, Square.DOUBLE_WORD);
+        this.setSquareSymetrical(2, 2, Square.DOUBLE_WORD);
+        this.setSquareSymetrical(3, 3, Square.DOUBLE_WORD);
+        this.setSquareSymetrical(4, 4, Square.DOUBLE_WORD);
+        this.setSquareSymetrical(5, 5, Square.TRIPLE_LETTER);
+        this.setSquareSymetrical(6, 6, Square.DOUBLE_LETTER);
+        this.setSquareSymetrical(5, 1, Square.TRIPLE_LETTER);
+        this.setSquareSymetrical(6, 2, Square.DOUBLE_LETTER);
+        this.setSquareSymetrical(7, 3, Square.DOUBLE_LETTER);
+    }
+
+    private void setSquareSymetrical(int x, int y, Square square) {
+        this.squares[y][x] = square;
+        this.squares[SIZE - 1 - y][x] = square;
+        this.squares[y][SIZE - 1 - x] = square;
+        this.squares[SIZE - 1 - y][SIZE - 1 - x] = square;
+
+        this.squares[x][y] = square;
+        this.squares[x][SIZE - 1 - y] = square;
+        this.squares[SIZE - 1 - x][y] = square;
+        this.squares[SIZE - 1 - x][SIZE - 1 - y] = square;
     }
 
     public void placeTile(Tile tile, int x, int y) {
