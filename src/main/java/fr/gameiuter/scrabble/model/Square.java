0b79ae1aa8ed3d15a7500ac3196f0b8577267f96
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-05-15 14:30:57 +0200

    add back all square types

--- src/main/java/fr/gameiuter/scrabble/model/Square.java
+++ src/main/java/fr/gameiuter/scrabble/model/Square.java
@@ -2,16 +2,15 @@ package fr.gameiuter.scrabble.model;
 
 public enum Square {
     NORMAL,
-    START;
-    // This is for later
-    // DOUBLE_LETTER,
-    // TRIPLE_LETTER,
-    // DOUBLE_WORD,
-    // TRIPLE_WORD;
+    START,
+    DOUBLE_LETTER,
+    TRIPLE_LETTER,
+    DOUBLE_WORD,
+    TRIPLE_WORD;
 
     public String symbol() {
         return switch (this) {
-            case NORMAL -> " ";
+            case NORMAL, DOUBLE_LETTER, TRIPLE_LETTER, DOUBLE_WORD, TRIPLE_WORD -> " ";
             case START -> "*";
         };
     }
