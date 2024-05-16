ac4bb19f9e33dd0a6f6d9fb0bed3a363d67356e5
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-05-16 11:03:04 +0200

    add a score to the player

--- src/main/java/fr/gameiuter/scrabble/model/Player.java
+++ src/main/java/fr/gameiuter/scrabble/model/Player.java
@@ -3,17 +3,27 @@ package fr.gameiuter.scrabble.model;
 public class Player {
     private final String name;
     private final Rack rack;
+    private int score;
 
     public Player(String name) {
         this.name = name;
         this.rack = new Rack();
+        this.score = 0;
     }
-    
+
     public String rackDisplay() {
         return this.rack.display();
     }
 
     public Rack getRack() {
         return this.rack;
+    }
+
+    public int score() {
+        return this.score;
+    }
+
+    public void incrementScore(int score) {
+        this.score += score;
     }
 }
