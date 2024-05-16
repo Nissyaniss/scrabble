25bdc119ed3734354ab0ae899435b970b3b6c29a
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-05-16 11:28:47 +0200

    tests base structure

---
+++ src/test/java/fr/gameiuter/scrabble/model/BoardTest.java
@@ -0,0 +1,12 @@
+package fr.gameiuter.scrabble.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+public class BoardTest {
+    @DisplayName("Tests for the score computing")
+    @Nested
+    class ScoreTest {
+        
+    }
+}
