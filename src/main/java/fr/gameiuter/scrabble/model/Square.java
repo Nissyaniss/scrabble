46fe813cc9571fd0c496d593bf04680938d3a8fd
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-05-16 10:48:14 +0200

    add multipliers in squares

--- src/main/java/fr/gameiuter/scrabble/model/Square.java
+++ src/main/java/fr/gameiuter/scrabble/model/Square.java
@@ -1,12 +1,20 @@
 package fr.gameiuter.scrabble.model;
 
 public enum Square {
-    NORMAL,
-    START,
-    DOUBLE_LETTER,
-    TRIPLE_LETTER,
-    DOUBLE_WORD,
-    TRIPLE_WORD;
+    NORMAL(1, 1),
+    START(1, 1),
+    DOUBLE_LETTER(2, 1),
+    TRIPLE_LETTER(3, 1),
+    DOUBLE_WORD(1, 2),
+    TRIPLE_WORD(1, 3);
+
+    private final int letterMultiplier;
+    private final int wordMultiplier;
+
+    Square(int letterMultiplier, int wordMultiplier) {
+        this.letterMultiplier = letterMultiplier;
+        this.wordMultiplier = wordMultiplier;
+    }
 
     public String symbol() {
         return switch (this) {
@@ -17,5 +25,13 @@ public enum Square {
             case DOUBLE_WORD -> "²";
             case TRIPLE_WORD -> "³";
         };
+    }
+
+    public int letterMultiplier() {
+        return this.letterMultiplier;
+    }
+
+    public int wordMultiplier() {
+        return this.wordMultiplier;
     }
 }
