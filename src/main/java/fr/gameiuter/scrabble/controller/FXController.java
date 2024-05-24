80cc215faa97e09aed3d4609b37277ae1b4d41db
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-05-24 09:57:10 +0200

    add fxcontroller

---
+++ src/main/java/fr/gameiuter/scrabble/controller/FXController.java
@@ -0,0 +1,9 @@
+package fr.gameiuter.scrabble.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class FXController {
+    @FXML
+    protected GridPane board;
+}
