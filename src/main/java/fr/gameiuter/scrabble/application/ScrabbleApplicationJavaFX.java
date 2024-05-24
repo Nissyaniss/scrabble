34c8f61083cf8eeca078afc5902bce7b1c865868
Yaniss Lasbordes <yaniss.lasbordes@gmail.com>
2024-05-24 09:50:51 +0200

    skeletton of javafx

--- src/main/java/fr/gameiuter/scrabble/application/ScrabbleApplicationJavaFX.java
+++ src/main/java/fr/gameiuter/scrabble/application/ScrabbleApplicationJavaFX.java
@@ -16,7 +16,7 @@ public class ScrabbleApplicationJavaFX extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ScrabbleApplicationJavaFX.class.getResource("/fr/gameiuter/scrabble/application/Application.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Scrabble");
         stage.setScene(scene);
         stage.show();
     }
