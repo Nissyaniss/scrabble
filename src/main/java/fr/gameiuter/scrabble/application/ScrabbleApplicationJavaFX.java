5e68205995898811dde12297d22b36f121af9b2f
Zacharie Dubrulle <dubrullezacharie@gmail.com>
2024-05-24 09:26:40 +0200

    javafx base

---
+++ src/main/java/fr/gameiuter/scrabble/application/ScrabbleApplicationJavaFX.java
@@ -0,0 +1,23 @@
+package fr.gameiuter.scrabble.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ScrabbleApplicationJavaFX extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ScrabbleApplicationJavaFX.class.getResource("/fr/gameiuter/scrabble/application/Application.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
