3c04e9de7cd591f9a78515955f28caeecb3dddc8
Yaniss Lasbordes <yaniss.lasbordes@gmail.com>
2024-06-05 09:32:52 +0200

    removing not useful variable

--- src/main/java/fr/gameiuter/scrabble/controller/FXStartMenuController.java
+++ src/main/java/fr/gameiuter/scrabble/controller/FXStartMenuController.java
@@ -20,9 +20,6 @@ public class FXStartMenuController {
     private Button startButton;
 
     @FXML
-    private Button leaveButton;
-
-    @FXML
     protected void checkName() {
         if (player1TextField.getText().isEmpty() || player2TextField.getText().isEmpty()) {
             startButton.setDisable(true);
@@ -33,7 +30,7 @@ public class FXStartMenuController {
 
     @FXML
     protected void handleStart() throws IOException {
-        FXMLLoader game = new FXMLLoader(FXStartMenuController.class.getResource("/fr/gameiuter/scrabble/application/Game.fxml"), null, null, (x) -> new FXGameController(player1TextField.getText(),player2TextField.getText()));
+        FXMLLoader game = new FXMLLoader(FXStartMenuController.class.getResource("/fr/gameiuter/scrabble/application/Game.fxml"), null, null, (x) -> new FXGameController(player1TextField.getText(), player2TextField.getText()));
         startButton.getScene().setRoot(game.load());
     }
 
